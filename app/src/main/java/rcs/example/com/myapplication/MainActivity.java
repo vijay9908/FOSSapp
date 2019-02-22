a1c46c8737e69fa289f578e6611160b81f84aa13
Yash Khare <yashsja@gmail.com>
2019-02-22 22:06:13 +0530

    App made.

--- app/src/main/java/rcs/example/com/myapplication/MainActivity.java
+++ app/src/main/java/rcs/example/com/myapplication/MainActivity.java
@@ -2,17 +2,24 @@ package rcs.example.com.myapplication;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent browserIntent = new Intent("android.intent.action.VIEW", Uri.parse("https://amfoss.in/"));
-        startActivity(browserIntent);
+        final Intent browserIntent = new Intent("android.intent.action.VIEW", Uri.parse("https://amfoss.in/"));
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(browserIntent);
+            }
+        }, 4000);
     }
 }
